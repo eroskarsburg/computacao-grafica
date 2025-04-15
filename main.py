ee37b5f87c928ac8dbe08cad03b28acf7796c7c4
vi <vivipilger12@gmail.com>
2025-04-14 22:15:09 -0300

    ajuste de função para modelagem de imagem com IA

--- main.py
+++ main.py
@@ -1,50 +1,25 @@
-import os
-from PIL import Image, ImageEnhance, ImageFilter
+import subprocess
 
-# Função para embelezar a imagem
-def beautify_image(image_path, output_folder, upscale_factor=4, sharpness_factor=2, blur_radius=1):
-    # Abrir a imagem
-    img = Image.open(image_path)
+def beautify_image(input_image, output_image, exe_path="./realesrgan-ncnn-vulkan/realesrgan-ncnn-vulkan.exe", model_name="realesrgan-x4plus"):
+    command = [
+        exe_path,
+        "-i", input_image,
+        "-o", output_image,
+        "-n", model_name
+    ]
 
-    # Redimensionar a imagem
-    width, height = img.size
-    new_width = int(width * upscale_factor)
-    new_height = int(height * upscale_factor)
-    img_resized = img.resize((new_width, new_height), Image.LANCZOS)
+    try:
+        subprocess.run(command, check=True, capture_output=True, text=True)
 
-    # Melhorar a nitidez
-    enhancer = ImageEnhance.Sharpness(img_resized)
-    img_sharpened = enhancer.enhance(sharpness_factor)
+    except subprocess.CalledProcessError as e:
+        print("Erro ao gerar imagem.")
+        print("Erro:", e.stderr)
 
-    # Aplicar suavização (filtro gaussiano)
-    img_smooth = img_sharpened.filter(ImageFilter.GaussianBlur(blur_radius))
+def main():
+    nome = "124"
+    input_path = f"C:\Repositories\computacao-grafica\PetImages\Dog\{nome}.jpg"
+    output_path = f"C:\Repositories\computacao-grafica\PetImagesBeautified\Dogs\{nome}_novo.jpg"
+    beautify_image(input_path, output_path)
 
-    # Verificar se a pasta de saída existe, se não, criar
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
-
-    # Criar o caminho de saída
-    output_path = os.path.join(output_folder, "beautified_image_smoothed.jpg")
-
-    # Salvar a imagem aprimorada
-    img_smooth.save(output_path)
-    print(f"Imagem suavizada e salva em: {output_path}")
-
-# Bloco principal para obter entrada do usuário e processar a imagem
 if __name__ == "__main__":
-    # Caminho da imagem original
-    image_path = "C:/Users/Eros/repos/computacao-grafica/PetImages/Cat/40.jpg"
-
-    # Verificar se o arquivo existe
-    if not os.path.exists(image_path):
-        print("O caminho especificado da imagem não existe. Verifique e tente novamente.")
-    else:
-        # Caminho da pasta de saída onde a imagem será salva
-        output_folder = "C:/Users/Eros/repos/computacao-grafica/PetImagesBeautified/Cats"
-
-        # Embelezar a imagem e criar a versão suavizada
-        beautify_image(image_path, output_folder)
-
-        # Opcional: Exibir a imagem para confirmar
-        img = Image.open(os.path.join(output_folder, "beautified_image_smoothed.jpg"))
-        img.show()
+    main()
