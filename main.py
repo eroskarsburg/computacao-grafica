517aa345c5cf389bf80da3114871f7bb9e81982b
eroskarsburg <eroskarsburg@gmail.com>
2025-04-21 21:05:24 -0300

    adicionado summary nos metodos e ajustado paths para retornar as imagens

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import subprocess
 import os
  
-BASE_INPUT_PATH = r"C:\Repositories\computacao-grafica\PetImages"
-BASE_OUTPUT_PATH = r"C:\Repositories\computacao-grafica\PetImagesBeautified"
+BASE_INPUT_PATH = rf".\PetImages"
+BASE_OUTPUT_PATH = rf".\PetImagesBeautified"
 ANIMAL_DIRS = {1: "Dog", 2: "Cat"}
 
 def beautify_image(input_image, output_image, exe_path="./realesrgan-ncnn-vulkan/realesrgan-ncnn-vulkan.exe", model_name="realesrgan-x4plus"):
+    """
+    Aplica melhoria de qualidade à imagem usando Real-ESRGAN.
+
+    Args:
+        input_image (str): Caminho para a imagem de entrada.
+        output_image (str): Caminho para salvar a imagem de saída.
+        exe_path (str): Caminho para o executável do Real-ESRGAN.
+        model_name (str): Nome do modelo a ser utilizado na melhoria.
+
+    Returns:
+        None
+    """
     command = [
         exe_path,
         "-i", input_image,
@@ -14,7 +26,7 @@ def beautify_image(input_image, output_image, exe_path="./realesrgan-ncnn-vulkan
     ]
 
     try:
-        result = subprocess.run(command, check=True, capture_output=True, text=True)
+        subprocess.run(command, check=True, capture_output=True, text=True)
         print("Imagem gerada com sucesso!")
 
         if os.path.exists(output_image):
@@ -25,6 +37,12 @@ def beautify_image(input_image, output_image, exe_path="./realesrgan-ncnn-vulkan
         print("Erro:", e.stderr)
 
 def get_animal_option():
+    """
+    Solicita ao usuário que selecione um tipo de animal (cachorro ou gato).
+
+    Returns:
+        int or None: Código do animal (1 para cachorro, 2 para gato), ou None se a entrada for inválida.
+    """
     print("Digite a opção do animal:")
     print("1. Cachorro")
     print("2. Gato")
@@ -39,6 +57,16 @@ def get_animal_option():
         return None
 
 def build_image_paths(animal_type, image_name):
+    """
+    Constrói os caminhos para a imagem de entrada e a imagem de saída com base no tipo de animal e nome da imagem.
+
+    Args:
+        animal_type (int): Código do animal (1 para cachorro, 2 para gato).
+        image_name (str): Nome da imagem (sem extensão).
+
+    Returns:
+        tuple: Caminhos completos para a imagem de entrada e de saída.
+    """    
     input_dir = os.path.join(BASE_INPUT_PATH, ANIMAL_DIRS[animal_type])
     output_dir = os.path.join(BASE_OUTPUT_PATH, ANIMAL_DIRS[animal_type] + "s")
 
